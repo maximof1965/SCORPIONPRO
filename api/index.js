bf68b87ae29acbf1c7e1f1020a1cc85afc64a111
nexumquantumsocial <maximof1965@gmail.com>
2026-05-01 23:32:04 -0500

    Revert "sync: ventas con turno manual en formulario, login SCORPION_APP_PASSWORD en API, sesiÃ³n sessionStorage (desde SCORPION UNIDO)"
    This reverts commit 34a1cca138cf254f84d0570bef179ba8c831bc6e.

--- api/index.js
+++ api/index.js
@@ -22,7 +22,6 @@ const handlers = {
   editar: require('../lib/handlers/editar'),
   recalcular_tot_in: require('../lib/handlers/recalcular_tot_in'),
   metricas: require('../lib/handlers/metricas'),
-  'app-login': require('../lib/handlers/app_login'),
 };
 
 module.exports = async (req, res) => {
